1e16e863ddf9afd8a63e000820669126b6a8ebc5
James Kyle <me@thejameskyle.com>
2014-04-18 21:49:12 -0400

    Fix browserify-shim

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-var Marionette = require('backbone.marionette');
 var $ = require('jquery');
+var _ = require('underscore');
+var Backbone = require('backbone');
+var Marionette = require('backbone.marionette');
 
 var Application = new Marionette.Application();
 
