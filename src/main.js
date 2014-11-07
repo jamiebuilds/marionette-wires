c2d0957af557d583175ff03658f2fd53322e064c
Jason Laster <jason.laster.11@gmail.com>
2014-11-07 12:07:41 -0500

    Update Marionette Wires to work with the inspector

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 require('./plugins');
 var Backbone = require('backbone');
+var Marionette = require('backbone.marionette');
+
+// start the marionette inspector
+if (window.__agent) {
+  window.__agent.start(Backbone, Marionette);
+}
+
 var Application = require('src/application/application');
 
 var app = new Application();
