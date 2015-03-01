a2cfc5983a2a28bb5fa9eb8bb7754bc3f8bfe641
Graeme Yeates <yeatesgraeme@gmail.com>
2015-03-01 13:46:37 -0500

    Update babel and less

--- src/plugins.js
+++ src/plugins.js
@@ -5,7 +5,7 @@ import Marionette from 'backbone.marionette';
 import 'bootstrap';
 import 'backbone.syphon';
 import 'backbone-query-parameters';
-import '6to5/polyfill';
+import 'babel/polyfill';
 
 // start the marionette inspector
 if (window.__agent) {
